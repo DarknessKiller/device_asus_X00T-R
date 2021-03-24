0ef19fa3efb0f67d22286a62a46589a8035e7c09
DarknessKiller <23330152+DarknessKiller@users.noreply.github.com>
2021-03-24 11:05:20 +0800

    Add Android Go Configs
    * Improvement toward 3GB without effecting ram management on higher end models.

--- device.mk
+++ device.mk
@@ -393,6 +393,14 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
+# Android GO Configs
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+
 # AsusClock & AsusCalculator & CameraGo
 PRODUCT_PACKAGES += \
     AsusClock \
